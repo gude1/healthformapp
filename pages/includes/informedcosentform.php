9e56580a9fcfcf2984a9c22f1a318f48428709d2
Owolabi Gideon <owolabigidslab@gmail.com>
2020-09-08 10:14:34 +0100

    done with informed consent moving to precounsellingform

--- pages/includes/informedcosentform.php
+++ pages/includes/informedcosentform.php
@@ -1,7 +1,48 @@
 <div class="w3-card w3-white  w3-padding-large healthforms" style="width:90%;height:90%;overflow: auto;">
-   		<h3 class="w3-center w3-bold">3.Informed Cosent</h3>
+   		<h3 class="w3-center w3-bold">3.Informed Consent</h3>
       <p>I hereby give cosent to:</p>
    	<ul class="">
    		<li>To receive comprehensive health services including TB & STI screenings</li>
+      <li>A Health Care Worker who will provide HTS to me & connect me to a health family</li>
+      <li>Allow SABCOHA to verify that i have reached referral family</li>
+      <li>Be contacted telephonically by SABCOHA on any of the numbers supplied for follow up</li>
    	</ul>
+    <p>I or a competent person on my behalf provide voluntary informed cosent that my information can be used for data collection and reporting,provided that confidentiality of my personal information is respected I/we had enough opportunity to ask questions</p>
+    <ul class="w3-ul">
+     <li class="healthformslist w3-border-0">
+          <div class="listitemcontainer">
+            <b>3.1 Consented to HIV Testing</b>
+          </div>
+              <div class="listitemcontainer">
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="consentedhivtesting" id="consentedhivtesting" value="yes">
+               <label for="consentedhivtesting">Yes</label><br>
+
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="consentedhivtesting" id="consentedhivtesting" value="no">
+               <label for="consentedhivtesting">No</label><br>
+              </div>
+          </li>
+
+          <li class="healthformslist w3-border-0">
+          <div class="listitemcontainer">
+            <b>3.2 DOH consent form signed by client</b>
+          </div>
+              <div class="listitemcontainer">
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="consentformsigned" id="consentformsigned" value="yes">
+               <label for="consentformsigned">Yes</label><br>
+
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="consentformsigned" id="consentformsigned" value="no">
+               <label for="consentformsigned">No</label><br>
+              </div>
+          </li>
+          <li class="healthformslist w3-border-0" style="flex-direction: row;align-items:center;justify-content:space-around; ">
+            <div class="w3-center">
+              <label for="signature">Signature:</label><br>
+             <input type="text" class="w3-input w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="signature" id="signature">
+             </div>
+            <div class="w3-center">
+                <label for="date">Date:</label><br>
+              <input type="date" class="w3-input w3-margin-left w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="date" id="date">
+            </div>
+          </li>
+    </ul>
 </div>
