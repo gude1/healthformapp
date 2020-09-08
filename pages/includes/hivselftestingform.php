cfa1bdce4b6dfcc2a9835016f709801846b986ca
Owolabi Gideon <owolabigidslab@gmail.com>
2020-09-08 15:08:42 +0100

    done wiht hivselftesting moving on to next file

--- pages/includes/hivselftestingform.php
+++ pages/includes/hivselftestingform.php
@@ -89,13 +89,89 @@
             <b>6.5 Distribution Channel</b>
           </div>
               <div class="listitemcontainer">
-               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="workabledistribution" id="workabledistribution" value="yes">
-               <label for="workabledistribution">Yes</label><br>
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="distributionchannel" id="distributionchannel" value="communityhotspot">
+               <label for="distributionchannel">Community Hotspot</label><br>
 
-              <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="workabledistribution" id="workabledistribution" value="no">
-               <label for="workabledistribution">No</label><br>
+              <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="distributionchannel" id="distributionchannel" value="facility">
+               <label for="distributionchannel">Facility</label><br>
+
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="distributionchannel" id="distributionchannel" value="workplace">
+               <label for="distributionchannel">Workplace</label><br>
+
+                <label for="distributionchannel">Others Specify :</label>
+               <input type="text" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="distributionchannel" id="distributionchannel"placeholder="others.">
+              </div>
+          </li> 
+
+           <li class="healthformslist w3-border-0">
+          <div class="listitemcontainer">
+            <b>6.6 HIVSS Demonstration</b>
+          </div>
+              <div class="listitemcontainer">
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="hivssdemonstration" id="hivssdemonstration" value="yes">
+               <label for="hivssdemonstration">Yes</label><br>
+
+              <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="hivssdemonstration" id="hivssdemonstration" value="no">
+               <label for="hivssdemonstration">No</label><br>
+              <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="hivssdemonstration" id="hivssdemonstration" value="no">
+               <label for="hivssdemonstration">No</label><br>
               </div>
           </li>  
 
+          <li class="healthformslist w3-border-0">
+          <div class="listitemcontainer">
+            <b>6.7 HIV self-screen kit for partner(S):</b><i class="w3-text-grey">Tick 1</i>
+          </div>
+              <div class="listitemcontainer">
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="screenkitforpartners" id="screenkitforpartners" value="yes">
+               <label for="screenkitforpartners">Yes</label><br>
+
+             <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="screenkitforpartners" id="screenkitforpartners" value="no">
+               <label for="screenkitforpartners">No</label><br>
+
+              <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="num" id="screenkitforpartners" value="notapplicable">
+               <label for="screenkitforpartners">Not applicable (if no partner)</label><br>
+
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="screenkitforpartners" id="screenkitforpartners" value="notoffered">
+               <label for="screenkitforpartners">Not offered</label><br>
+
+                <label><b>Number of partner(s)</b></label><br>
+                <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="numpartners" id="numpartners" value="1">
+               <label for="screenkitforpartners">1</label><br>
+
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="numpartners" id="numpartners" value="2">
+               <label for="screenkitforpartners">2</label><br>
+
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="numpartners" id="numpartners" value="3 & more">
+               <label for="screenkitforpartners">3 & more</label><br>
+              </div>
+          </li>   
+
+          <li class="healthformslist w3-border-0">
+          <div class="listitemcontainer">
+            <b>6.8 Gender of partner:</b><i class="w3-text-grey">Tick 1</i>
+          </div>
+              <div class="listitemcontainer">
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="partnergender" id="partnergender" value="male">
+               <label for="partnergender">Male</label><br>
+             <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="partnergender" id="partnergender" value="transgender">
+               <label for="partnergender">Transgender</label><br>
+
+             <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="partnergender" id="partnergender" value="notidentify">
+              <label for="partnergender">Do not identify as Male ,Female,TG</label><br>
+              </div>
+            </li>
+
+            <li class="healthformslist w3-border-0">
+          <div class="listitemcontainer">
+            <b>6.9 Age of partner:</b><i class="w3-text-grey">(Years of Date of Birth)</i>
+          </div>
+              <div class="listitemcontainer">
+               <input type="number" class="w3-input w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="partnerage" id="partnerage" placeholder="age">
+               <label><b>Date of Birth</b></label>
+             <input type="date" class="w3-input w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="partnerage" id="partnerage" placeholder="date">
+              </div>
+            </li>
+             
    	</ul>
 </div>
