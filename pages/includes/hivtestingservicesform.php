dae834cb7fe7202ddd6009349ced39efbf3c9172
Owolabi Gideon <owolabigidslab@gmail.com>
2020-09-08 12:32:33 +0100

    done with hivtestingservicesform moving to Hivselftesting form

--- pages/includes/hivtestingservicesform.php
+++ pages/includes/hivtestingservicesform.php
@@ -1,4 +1,4 @@
-<div class="w3-card w3-white  w3-padding-large  healthforms" style="width:90%;height:90%;overflow: auto;">
+<div class="w3-card w3-white  w3-padding-large  healthforms w3-hide" style="width:90%;height:90%;overflow: auto;">
    		<h3 class="w3-center w3-bold">5.Hiv Testing Services</h3>
    	<ul class="w3-ul">
          <li class="healthformslist w3-border-0">
@@ -53,6 +53,40 @@
                <label for="confirmatorytestcomplete">Inconclusive</label><br>
               </div>
           </li>
+
+          <li class="healthformslist w3-border-0">
+          <div class="listitemcontainer">
+            <b>5.3 if HIV +ve</b>
+          </div>
+              <div class="listitemcontainer">
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="hiv+ve" id="hiv+ve" value="known+vereferredart">
+               <label for="hiv+ve">Known +ve Referred ART</label><br>
+
+             <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="hiv+ve" id="hiv+ve" value="known+veinitiatedart">
+               <label for="hiv+ve">Known +ve Initiated ART</label><br>
+
+               <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="hiv+ve" id="hiv+ve" value="tested+vereferredart">
+               <label for="hiv+ve">Tested +ve Referred ART</label><br>
+
+                <input type="radio" class="w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="hiv+ve" id="hiv+ve" value="tested+veinitiatedart">
+               <label for="hiv+ve">Tested +ve Initiated ART</label><br>
+              </div>
+          </li> 
+
+           <li class="healthformslist w3-border-0">
+          <div class="listitemcontainer">
+            <b>5.4 Name of Test</b>
+          </div>
+              <div class="listitemcontainer">
+               <input type="text" class="w3-input w3-padding w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="testname" id="testname" placeholder="test name"><br>
+
+               <label for="lotbatchnumber"><b>Lot/Batch Number</b></label>
+               <input type="text" class="w3-input w3-padding w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="lotbatchnumber" id="lotbatchnumber" placeholder="lot/batch number"><br>
+              <label for="expirydate"><b>Expiry Date</b></label>
+               <input type="date" class="w3-input w3-padding w3-border-0 w3-border-bottom w3-border-blue w3-hover-border-green" name="expirydate" id="expirydate" placeholder="expirydate"><br>
+
+              </div>
+          </li> 
 
    	</ul>
 </div>
